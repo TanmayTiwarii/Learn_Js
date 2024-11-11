56998f5693b39b040126e90f6f0dec85bb231095
Tanmay5454 <tiwaritanmay424@gmail.com>
2024-11-11 22:53:11 +0530

    learned data types

---
+++ 01_basics/variables.js
@@ -0,0 +1,28 @@
+
+let accountId = "11234134"
+
+var age = 20
+//var should not be used as it has scope issues
+
+console.log(accountId) 
+
+
+console.table([accountId,age])
+// this is good for seeing multple outputs
+
+
+// data types
+
+let a = "adas" // string
+
+let b = 123    // number/int
+
+let c = true   // boolean
+
+let d = null   // object because when typeof is called it shows object
+
+let e          // undefined
+
+console.log(typeof d)
+
+console.log(typeof e)
